35e6d99e8daefeeee76b968c5d546c0e2ed748b2
Hlib Liapota <hlib.liapota@e-hps.com>
2025-09-01 16:54:01 +0200

    feat: implement task list UI with Angular Material components

--- todolist-again-front/src/app/components/task-list/task-list.component.ts
+++ todolist-again-front/src/app/components/task-list/task-list.component.ts
@@ -1,13 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Task } from '../../models/task';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatListOption, MatSelectionList } from '@angular/material/list';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatFormField, MatInput, MatSelectionList, MatListOption],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TaskListComponent { 
+export class TaskListComponent {
   tasks: Task[] = [];
 }

6fa6100b7138b4bc1288a6dbfb7a973d73dbecce
Hlib Liapota <hlib.liapota@e-hps.com>
2025-09-03 16:58:01 +0200

    feat: enhance task list component with improved UI and functionality

--- todolist-again-front/src/app/components/task-list/task-list.component.ts
+++ todolist-again-front/src/app/components/task-list/task-list.component.ts
@@ -4,14 +4,42 @@ import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/m
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatListOption, MatSelectionList } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task-list',
-  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatFormField, MatInput, MatSelectionList, MatListOption],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatSelectionList,
+    MatListOption,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskListComponent {
-  tasks: Task[] = [];
+  toggleTask(arg0: number) {
+    this.tasks = this.tasks.map(task => {
+      if (task.id === arg0) {
+        return { ...task, isDone: !task.isDone };
+      }
+      return task;
+    });
+  }
+  logTaskList() {
+    console.log(this.tasks);
+  }
+  tasks: Task[] = [
+    { id: 1, title: 'Task 1', isDone: false },
+    { id: 2, title: 'Task 2', isDone: true },
+    { id: 3, title: 'Task 3', isDone: false },
+  ];
 }

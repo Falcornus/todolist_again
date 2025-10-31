f85baa8df5ba9dd9b26cdb85def30273d77a21b7
Hlib Liapota <hlib.liapota@e-hps.com>
2025-10-31 12:28:56 +0100

    feat: add README documentation and update task management to use backend for task operations

--- todolist-again-front/src/app/components/task-list/task-list.component.ts
+++ todolist-again-front/src/app/components/task-list/task-list.component.ts
@@ -36,44 +36,38 @@ import { Observable } from 'rxjs';
 export class TaskListComponent {
   private tasksService = inject(TasksService);
 
-  public localTasks: Task[] = [
-    { id: 1, title: 'Task 1', isDone: false },
-    { id: 2, title: 'Task 2', isDone: true },
-    { id: 3, title: 'Task 3', isDone: false },
-  ];
-
   public tasks: Observable<Task[]> = this.tasksService.getTasks();
 
   public taskControl = new FormControl('');
 
   public toggleTask(id: number) {
-    this.localTasks = this.localTasks.map(task => {
-      if (task.id === id) {
-        return { ...task, isDone: !task.isDone };
-      }
-      return task;
-    });
+    // this.localTasks = this.localTasks.map(task => {
+    //   if (task.id === id) {
+    //     return { ...task, isDone: !task.isDone };
+    //   }
+    //   return task;
+    // });
   }
 
   public addTask() {
     const taskTitle = this.taskControl.value?.trim();
     if (taskTitle) {
       const newTask: Task = {
-        id: this.getNextId(),
+        id: 0, // Backend will assign the actual ID
         title: taskTitle,
         isDone: false
       };
-      this.localTasks = [...this.localTasks, newTask];
-      this.taskControl.reset();
+
+      console.log('Adding task:', newTask);
+      this.tasksService.addTask(newTask).subscribe(() => {
+        this.taskControl.reset();
+        // Refresh the tasks list to include the newly added task
+        this.tasks = this.tasksService.getTasks();
+      });
     }
   }
 
   public removeTask(id: number) {
-    this.localTasks = this.localTasks.filter(task => task.id !== id);
-  }
-
-  private getNextId(): number {
-    return this.localTasks.length > 0 ? Math.max(...this.localTasks.map(t => t.id)) + 1 : 1;
+    // this.localTasks = this.localTasks.filter(task => task.id !== id);
   }
 }
-  
